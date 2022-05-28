bba031f8708e117699f64b2872651a1c777906b8
Dylan Yates <dylan_yates1@homedepot.com>
2022-05-28 08:01:40 -0500

    nuke

--- src/App.js
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div>
-      Dylan Yates
-    </div>
-  );
-}
-
-export default App;
--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders my name on screen', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Dylan Yates/i);
-  expect(textElement).toBeInTheDocument();
-});
