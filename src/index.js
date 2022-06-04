ca36ebb99b5cb35299968c0f55cadaefaf36ec76
Dylan Yates <dylan_yates1@homedepot.com>
2022-06-04 14:12:44 -0500

    remove react router dom

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<div>Hello World</div>} />
-        <Route path="/casino" element={<App />} />
-        <Route path="/:null" element={<div>404 Not Found</div>} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
